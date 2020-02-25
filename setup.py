dc8e080edf96c90a86f5e6001b0450cf5e9a6eb4
Dillan Teagle <dteagle@cpicardgroup.com>
2020-02-25 11:05:50 -0600

    :package: configure packaging source distribution for automation

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import versioneer
 from setuptools import setup, find_packages
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
+
 
 CWD = abspath(dirname(__file__))
 
+with open(join(CWD, "requirements.txt"), encoding="utf-8") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+with open(join(CWD, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="robotframework-seleniumproxy",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Capture requests/responses generated with Seleniums Webdriver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Dillan Teagle",
     author_email="dillan@teaglebuilt.com",
     url="https://github.com/teaglebuilt/robotframework-seleniumproxy",
@@ -14,15 +26,14 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework:: Robot Framework',
+        'Framework:: Robot Framework:: Library'
     ],
-    description="Extends Selenium to give you the ability to inspect requests made by the browser.",
-    install_requires=['selenium>=3.4.0'],
+    install_requires=REQUIREMENTS,
+    keywords="selenium webdriver proxy robotframework seleniumlibrary network activity request response",
     license="MIT",
     packages=find_packages("src"),
-    package_dir={'': 'src'},
-    version='0.0.1'
+    package_dir={'': 'src'}
 )
--- tasks.py
+++ tasks.py
@@ -1,4 +1,27 @@
 from invoke import task
+from pathlib import Path
+import os
+import os.path
+import shutil
+import glob
+
+
+CHANGELOG = "CHANGELOG"
+filters = ["poc", "new release", "wip", "cleanup", "!nocl"]
+
+
+def filter_entries(filename):
+    buffer = []
+    with open(filename) as old_file:
+        buffer = old_file.read().split("\n")
+
+    with open(filename, "w") as new_file:
+        for line in buffer:
+            if not any(bad_word in line.lower() for bad_word in filters):
+                new_file.write(line + "\n")
+
+
+assert Path.cwd() == Path(__file__).parent
 
 
 @task
@@ -20,3 +43,64 @@ def lint(ctx):
 def docs(ctx):
     ctx.run("python -m robot.libdoc --pythonpath src SeleniumProxy  docs/keywords.html")
     ctx.run("cp docs/keywords.html docs/index.html")
+
+
+@task
+def compileDev(ctx):
+    ctx.run("pip-compile --output-file=requirements-dev.txt dependencies/requirements-dev.in")
+
+
+@task
+def compileProd(ctx):
+    ctx.run("pip-compile --output-file=requirements.txt dependencies/requirements.in")
+
+
+@task
+def clean(ctx):
+    to_be_removed = [
+        "coverage_report/",
+        "reports/",
+        "src/robotframework_seleniumtestability.egg-info/",
+        "dist/",
+        "output.xml",
+        ".coveragedb",
+        "*.html",
+        "selenium-screenshot-*.png",
+        "geckodriver.log",
+        "SeleniumProxy.log",
+    ]
+
+    for item in to_be_removed:
+        if os.path.isdir(item):
+            shutil.rmtree(item)
+        elif os.path.isfile(item):
+            os.remove(item)
+        else:
+            for filename in glob.glob(item):
+                os.remove(filename)
+
+
+@task
+def build(ctx):
+    ctx.run("python setup.py sdist")
+
+
+@task
+def changelog(ctx, version=None):
+    if version is not None:
+        version = "-c {}".format(version)
+    else:
+        version = ""
+    ctx.run("gcg  -x -o {} -O rpm  {}".format(CHANGELOG, version))
+    filter_entries(CHANGELOG)
+
+
+@task
+def release(ctx, version=None):
+    assert version != None
+    changelog(ctx, version)
+    docs(ctx)
+    ctx.run("git add docs/* {}".format(CHANGELOG))
+    ctx.run("git commit -m 'New Release {}'".format(version))
+    ctx.run("git tag {}".format(version))
+    build(ctx)
